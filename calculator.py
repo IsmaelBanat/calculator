f70f483460841f6a3c7e65c0b26bc5b9bcef7b77
Ismael Banat <ismailbanat60@gmail.com>
2024-02-17 13:41:47 +0200

    Update calculator.py
    implemented loop for calculator

--- calculator.py
+++ calculator.py
@@ -1,18 +1,25 @@
 from math import sqrt, pi
 print('SIMPLE CALCULATOR')
-operator = input('Choose operator (+, -, x, /, sqrt): ')
-num_1 = float(input('num_1 is: '))
-num_2 = float(input('num_2 is: '))
-try:
-    if operator == '+':
-        print(num_1+num_2)
-    if operator == '-':
-        print(num_1-num_2)
-    if operator == 'x':
-        print(num_1*num_2)
-    if operator == '/':
-        print(num_1/num_2)
-    if operator == 'sqrt':
-        print(sqrt(num_1))
-except:
-    print('Invalid operator!')
+while True:
+    status = input("Type 'start' or 'end': ")
+    if status == "Start" or "start":
+        operator = input('Choose operator (+, -, x, /, sqrt): ')
+        num_1 = float(input('num_1 is: '))
+        num_2 = float(input('num_2 is: '))
+        try:
+            if operator == '+':
+                print(num_1+num_2)
+            if operator == '-':
+                print(num_1-num_2)
+            if operator == 'x':
+                print(num_1*num_2)
+            if operator == '/':
+                print(num_1/num_2)
+            if operator == 'sqrt':
+                print(sqrt(num_1))
+        except:
+            print('Invalid operator!')
+    elif status == "End" or "end":
+        break
+    else:
+        print("Invalid choice! Choose either 'Start' or 'End'")
